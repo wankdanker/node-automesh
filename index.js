07544a3accc8e6f1b57e2c65d4f959a07b14ea59
Dan VerWeire <dverweire@gmail.com>
2015-01-29 22:20:21 -0500

    Add options.client and options.server options

--- index.js
+++ index.js
@@ -11,32 +11,48 @@ module.exports = function (options) {
 
 function AutoMesh (options) {
 	EventEmitter.call(this);
+	options = options || {};
 
 	var self = this;
 	var d = self.discover = discover(options);
+	var doListen = options.server || (!options.server && !options.client);
+	var doConnect = options.client || (!options.server && !options.client);
 
-	d.on('added', function (node) {
-		node.connection = net.createConnection(node.advertisement.port, node.address, function () {
-			self.emit("outbound", node.connection, node);	
+	if (doConnect) {
+		d.on('added', function (node) {
+			if (!node.advertisement.port) {
+				//remote node is not advertising a port
+				//it must not be listening.
+				return;
+			}
+
+			//connect to the newly discovered node
+			node.connection = net.createConnection(node.advertisement.port, node.address, function () {
+				self.emit("outbound", node.connection, node);	
+				self.emit("server", node.connection, node);	
+			});
 		});
-	});
+	}
 
-	//create a tcp server
-	var server = net.createServer(function (c) {
-		self.emit("inbound", c);
-	});
+	if (doListen) {
+		//create a tcp server
+		var server = net.createServer(function (c) {
+			self.emit("inbound", c);
+			self.emit("client", c);
+		});
 
-	//get a random local port to listen on
-	getPort(function (err, port) {
-		if (err) {
-			return console.log(err);
-		}
+		//get a random local port to listen on
+		getPort(function (err, port) {
+			if (err) {
+				return console.log(err);
+			}
 
-		server.listen(port);
+			server.listen(port);
 
-		//advertise what port we are listening on
-		d.advertise({ port : port });
-	});
+			//advertise what port we are listening on
+			d.advertise({ port : port });
+		});
+	}
 };
 
 inherits(AutoMesh, EventEmitter);
